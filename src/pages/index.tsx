d873aa21b75fc058f1a9255493ed683054b37d42
Diego Aquino <diego.cruz001@gmail.com>
2020-12-07 11:40:50 -0300

    Wrap Home page's elements with Layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,11 @@
 import React, { FC } from 'react';
-import Head from 'next/head';
 
-import Logo from '../assets/Logo';
+import { Layout } from 'components/common';
 
 const Home: FC = () => (
-  <div>
-    <Head>
-      <title>TheCountdown</title>
-    </Head>
-
-    <main>
-      TheCountdown
-      <Logo />
-    </main>
-  </div>
+  <Layout pageTitle="TheCountdown">
+    <main />
+  </Layout>
 );
 
 export default Home;
