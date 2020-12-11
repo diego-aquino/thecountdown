667e10dd1c0d1108f84c87536580c6e299cffb5c
Diego Aquino <diego.cruz001@gmail.com>
2020-12-11 19:43:59 -0300

    Update arrow icon to contain only an arrow head

--- src/assets/Arrow.tsx
+++ src/assets/Arrow.tsx
@@ -4,34 +4,22 @@ import { HTMLSvgElementProps } from 'typings';
 
 const Arrow: FC<HTMLSvgElementProps> = ({ ...rest }) => (
   <svg
-    width="133"
+    width="8"
     height="12"
-    viewBox="0 0 133 12"
+    viewBox="0 0 8 12"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     {...rest}
   >
-    <rect y="5" width="130" height="2" fill="url(#paint0_linear)" />
     <g clipPath="url(#clip0)">
       <path
-        d="M126.427 0L125 1.41L129.636 6L125 10.59L126.427 12L132.5 6L126.427 0Z"
+        d="M1.42713 0L0 1.41L4.63563 6L0 10.59L1.42713 12L7.5 6L1.42713 0Z"
         fill="#1C5DC4"
       />
     </g>
     <defs>
-      <linearGradient
-        id="paint0_linear"
-        x1="9.68576e-07"
-        y1="6"
-        x2="130"
-        y2="6.00005"
-        gradientUnits="userSpaceOnUse"
-      >
-        <stop stopColor="#2094CA" />
-        <stop offset="1" stopColor="#1C5DC4" />
-      </linearGradient>
       <clipPath id="clip0">
-        <rect width="7.5" height="12" fill="white" transform="translate(125)" />
+        <rect width="7.5" height="12" fill="white" />
       </clipPath>
     </defs>
   </svg>
