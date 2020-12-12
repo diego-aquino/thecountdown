a9133c0bf9d845796527c4e4475ec25a6955ffd8
Diego Aquino <diego.cruz001@gmail.com>
2020-12-12 08:26:20 -0300

    Add MainCountdown to the home page and center it

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import React, { FC } from 'react';
 
 import { Layout } from 'components/common';
+import { MainCountdown } from 'components/countdown';
+import styles from 'styles/components/pages/Home.module.css';
 
 const Home: FC = () => (
   <Layout pageTitle="TheCountdown">
-    <main />
+    <main className={styles.main}>
+      <MainCountdown />
+    </main>
   </Layout>
 );
 
