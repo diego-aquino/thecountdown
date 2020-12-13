add6dce5800267a3ab988b41084487dc04a48da2
Diego Aquino <diego.cruz001@gmail.com>
2020-12-13 16:42:31 -0300

    Call useResize callback on mount and on resize

--- src/hooks/useResize.tsx
+++ src/hooks/useResize.tsx
@@ -1,9 +1,11 @@
 import { useEffect } from 'react';
 
-type Callback = (event: UIEvent) => void;
+type Callback = (event?: UIEvent) => void;
 
 function useResize(callback: Callback): void {
   useEffect(() => {
+    callback();
+
     window.addEventListener('resize', callback);
 
     return () => window.removeEventListener('resize', callback);
