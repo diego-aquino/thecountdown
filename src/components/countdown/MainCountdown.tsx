5a63424d1922fc73852ddf342066c1624d7b0c62
Diego Aquino <diego.cruz001@gmail.com>
2020-12-14 18:10:10 -0300

    Initiate times as null and update them on mount

--- src/components/countdown/MainCountdown.tsx
+++ src/components/countdown/MainCountdown.tsx
@@ -1,19 +1,25 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 
 import { Time } from 'typings';
+import { CircularLoading } from 'components/common';
 import { getFutureTime } from 'utils/date';
 import styles from 'styles/components/countdown/MainCountdown.module.css';
 import CountdownTimer from './CountdownTimer';
 import TimeRangeForm from './TimeRangeForm';
 
 const MainCountdown: FC = () => {
-  const [startTime, setStartTime] = useState<Time>({ refersToNow: true });
-  const [endTime, setEndTime] = useState<Time>(
-    getFutureTime({
-      dayVariation: 48,
-      hoursVariation: 11,
-    }),
-  );
+  const [startTime, setStartTime] = useState<Time | null>(null);
+  const [endTime, setEndTime] = useState<Time | null>(null);
+
+  useEffect(() => {
+    setStartTime({ refersToNow: true });
+    setEndTime(
+      getFutureTime({
+        dayVariation: 48,
+        hoursVariation: 11,
+      }),
+    );
+  }, []);
 
   return (
     <div className={styles.mainCountdown}>
