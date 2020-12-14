9006d9d6c67f38750adc1dc0b27ab8cb91294491
Diego Aquino <diego.cruz001@gmail.com>
2020-12-14 13:42:36 -0300

    Use localStorage to save the last entered times

--- src/components/countdown/MainCountdown.tsx
+++ src/components/countdown/MainCountdown.tsx
@@ -1,19 +1,40 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 
 import { Time } from 'typings';
-import { getFutureTime } from 'utils/date';
+import {
+  getFutureTime,
+  getLastTimeEntryFor,
+  saveTimeEntryLocally,
+} from 'utils/date';
 import styles from 'styles/components/countdown/MainCountdown.module.css';
 import CountdownTimer from './CountdownTimer';
 import TimeRangeForm from './TimeRangeForm';
 
+const defaultStartTime: Time = { refersToNow: true };
+const defaultEndTime: Time = getFutureTime({
+  dayVariation: 48,
+  hoursVariation: 11,
+});
+
+const lastStartTimeEntry = getLastTimeEntryFor('startTime');
+const lastEndTimeEntry = getLastTimeEntryFor('endTime');
+
 const MainCountdown: FC = () => {
-  const [startTime, setStartTime] = useState<Time>({ refersToNow: true });
-  const [endTime, setEndTime] = useState<Time>(
-    getFutureTime({
-      dayVariation: 48,
-      hoursVariation: 11,
-    }),
-  );
+  const [startTime, setStartTime] = useState<Time>(defaultStartTime);
+  const [endTime, setEndTime] = useState<Time>(defaultEndTime);
+
+  useEffect(() => {
+    setStartTime(lastStartTimeEntry || defaultStartTime);
+    setEndTime(lastEndTimeEntry || defaultEndTime);
+  }, []);
+
+  useEffect(() => {
+    saveTimeEntryLocally('startTime', startTime);
+  }, [startTime]);
+
+  useEffect(() => {
+    saveTimeEntryLocally('endTime', endTime);
+  }, [endTime]);
 
   return (
     <div className={styles.mainCountdown}>
