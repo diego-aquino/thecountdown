d2ed05b2fd1405173690a6c8167cdaed95235301
Diego Aquino <diego.cruz001@gmail.com>
2020-12-15 09:23:30 -0300

    Abstract time inputs to an independent component

--- src/components/countdown/TimeRangeForm.tsx
+++ src/components/countdown/TimeRangeForm.tsx
@@ -1,26 +1,22 @@
-import React, { FC, useCallback, useMemo } from 'react';
-import DateTimePicker from 'react-datetime-picker/dist/entry.nostyle';
+import React, { FC, useMemo } from 'react';
 import clsx from 'clsx';
 
 import { HTMLFormElementProps, Time } from 'typings';
-import { Calendar, Hourglass, XMark } from 'assets';
+import { Hourglass } from 'assets';
 import { AnimatedArrow } from 'components/common';
 import { useScreenBreakpoint } from 'hooks';
 import styles from 'styles/components/countdown/TimeRangeForm.module.css';
-
-interface ActiveStartDateChangeEvent {
-  activeStartDate: Date;
-}
-
-type onChangeTimeCallback = (newStartTime: Time) => void;
+import TimeInput from './TimeInput';
 
 type FormLayout = 'horizontal' | 'vertical';
+
+type OnChangeTimeCallback = (newTime: Time) => void;
 
 interface Props extends HTMLFormElementProps {
   startTime: Time;
   endTime: Time;
-  onStartTimeChange?: onChangeTimeCallback;
-  onEndTimeChange?: onChangeTimeCallback;
+  onStartTimeChange?: OnChangeTimeCallback;
+  onEndTimeChange?: OnChangeTimeCallback;
   layoutBreakpoint?: number;
 }
 
@@ -37,35 +33,6 @@ const TimeRangeForm: FC<Props> = ({
   const layout = useScreenBreakpoint<FormLayout>(
     ['vertical', 'horizontal'],
     [layoutBreakpoint],
-  );
-
-  const handleDateChange = useCallback(
-    (onChange: onChangeTimeCallback, newDate: Date | null) => {
-      const newTime: Time = newDate
-        ? { refersToNow: false, date: newDate }
-        : { refersToNow: true };
-
-      onChange(newTime);
-    },
-    [],
-  );
-
-  const handleStartDateChange = useCallback(
-    (newStartDate: Date | null) => {
-      if (onStartTimeChange) {
-        handleDateChange(onStartTimeChange, newStartDate);
-      }
-    },
-    [handleDateChange, onStartTimeChange],
-  );
-
-  const handleEndDateChange = useCallback(
-    (newEndDate: Date | null) => {
-      if (onEndTimeChange) {
-        handleDateChange(onEndTimeChange, newEndDate);
-      }
-    },
-    [handleDateChange, onEndTimeChange],
   );
 
   const isStartTimeLaterThanEndTime = useMemo(() => {
@@ -85,58 +52,12 @@ const TimeRangeForm: FC<Props> = ({
       onSubmit={onSubmit}
       {...rest}
     >
-      <div className={styles.startTimeInputContainer}>
-        <button
-          className={clsx(
-            styles.nowLabelContainer,
-            !startTime.refersToNow && styles.hidden,
-          )}
-          type="button"
-          onClick={() => handleStartDateChange(new Date())}
-        >
-          <h4 className={styles.nowLabel}>
-            <Calendar
-              id="startTimeCalendarIcon"
-              title="Calendar"
-              className={styles.calendarIcon}
-            />
-            Now
-          </h4>
-          <XMark
-            id="clearStartTimeNowLabel"
-            title="Clear"
-            className={styles.XMark}
-          />
-        </button>
-
-        <DateTimePicker
-          className={clsx(
-            styles.dateTimePicker,
-            startTime.refersToNow && styles.hidden,
-          )}
-          calendarIcon={
-            <Calendar
-              id="openStartTimeCalendar"
-              title="Open calendar"
-              className={styles.calendarIcon}
-            />
-          }
-          clearIcon={
-            <XMark
-              id="clearStartTimeInputs"
-              title="Clear"
-              className={styles.XMark}
-            />
-          }
-          showLeadingZeros
-          value={startTime.refersToNow ? null : startTime.date}
-          activeStartDate={startTime.refersToNow ? null : startTime.date}
-          onChange={handleStartDateChange}
-          onActiveStartDateChange={(
-            { activeStartDate }: ActiveStartDateChangeEvent, // eslint-disable prettier/prettier
-          ) => handleStartDateChange(activeStartDate)}
-        />
-      </div>
+      <TimeInput
+        className={styles.startTimeInput}
+        id="startTimeInput"
+        timeValue={startTime}
+        onTimeChange={onStartTimeChange}
+      />
 
       <div className={styles.middleIconsContainer}>
         <AnimatedArrow
@@ -149,58 +70,12 @@ const TimeRangeForm: FC<Props> = ({
         <Hourglass className={styles.hourglass} />
       </div>
 
-      <div className={styles.endTimeInputContainer}>
-        <button
-          className={clsx(
-            styles.nowLabelContainer,
-            !endTime.refersToNow && styles.hidden,
-          )}
-          type="button"
-          onClick={() => handleEndDateChange(new Date())}
-        >
-          <h4 className={styles.nowLabel}>
-            <Calendar
-              id="endTimeCalendarIcon"
-              title="Calendar"
-              className={styles.calendarIcon}
-            />
-            Now
-          </h4>
-          <XMark
-            id="clearEndTimeNowLabel"
-            title="Clear"
-            className={styles.XMark}
-          />
-        </button>
-
-        <DateTimePicker
-          className={clsx(
-            styles.dateTimePicker,
-            endTime.refersToNow && styles.hidden,
-          )}
-          calendarIcon={
-            <Calendar
-              id="openEndTimeCalendar"
-              title="Open calendar"
-              className={styles.calendarIcon}
-            />
-          }
-          clearIcon={
-            <XMark
-              id="clearEndTimeInputs"
-              title="Clear"
-              className={styles.XMark}
-            />
-          }
-          showLeadingZeros
-          value={endTime.refersToNow ? null : endTime.date}
-          activeStartDate={endTime.refersToNow ? null : endTime.date}
-          onChange={handleEndDateChange}
-          onActiveStartDateChange={(
-            { activeStartDate }: ActiveStartDateChangeEvent, // eslint-disable prettier/prettier
-          ) => handleEndDateChange(activeStartDate)}
-        />
-      </div>
+      <TimeInput
+        className={styles.endTimeInput}
+        id="endTimeInput"
+        timeValue={endTime}
+        onTimeChange={onEndTimeChange}
+      />
     </form>
   );
 };
--- src/components/countdown/index.tsx
+++ src/components/countdown/index.tsx
@@ -1,4 +1,6 @@
 import MainCountdown from './MainCountdown';
 import CountdownTimer from './CountdownTimer';
+import TimeRangeForm from './TimeRangeForm';
+import TimeInput from './TimeInput';
 
-export { MainCountdown, CountdownTimer };
+export { MainCountdown, CountdownTimer, TimeRangeForm, TimeInput };
