7b06714bfe15e49a59639320beea92f7e8d51dd2
Diego Aquino <diego.cruz001@gmail.com>
2020-12-17 14:34:52 -0300

    Replace tag verification method by HTML file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -44,10 +44,6 @@ class GlobalDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap"
             rel="stylesheet"
           />
-          <meta
-            name="google-site-verification"
-            content="tY6Tt0X1inh4u36gOc99yz-HH5w36zXDG95kEy8-2Fc"
-          />
         </Head>
 
         <body>
